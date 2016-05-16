6eb319e3bec49629a0d711480a19eca933a3712d
lk1926 <lk1926@wp.pl>
2016-05-16 10:27:42 +0200

    prawidłowy ruch

--- OiX.py
+++ OiX.py
@@ -57,5 +57,22 @@ def nowa_plansza():
         plansza.append(puste)
     return plansza
 
+def wyswietlona_plansz(plansza):
+    """Wyświetla planszę gry na ekranie."""
+    print("\n\t", plansza[0], "|", plansza[1], "|", plansza[2])
+    print("\t", "---------")
+    print("\t", plansza[3], "|", plansza[4], "|", plansza[5])
+    print("\t", "---------")
+    print("\t", plansza[6], "|", plansza[7], "|", plansza[8], "\n")
+
+
+def prawidlowy_ruch(plansza):
+    """Lista prawidłowych ruchów."""
+    lista_ruchow = []
+    for pole in range(liczba_pol):
+        if plansza[pole] == puste:
+            lista_ruchow.append(pole)
+    return lista_ruchow
+
 
 
